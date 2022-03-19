62b6a54e0346e6ad521bec97906e5c87be45386e
angelayalafinance <96507984+angelayalafinance@users.noreply.github.com>
2022-03-18 20:06:22 -0400

    Update sentimentanalyzer.py

--- sentimentanalyzer.py
+++ sentimentanalyzer.py
@@ -1,1 +1,36 @@
 
+
+
+
+
+
+
+#   Function that parses file into words, passing the file as the argument
+from numpy import array
+
+
+def filereader(articles):
+    with open(articles) as file:
+    # reading each line    
+    for line in file:
+   
+        # reading each word        
+        for word in line.split():
+   
+          #populate into an array
+
+#   Function that counts number of positive words, passing word file array
+def scorecount(arr):
+    for i in array:
+        # compare word with words
+            # increment count
+
+        # return integer
+
+
+
+
+
+
+
+#   Choose the top 5 stocks
