68a7742fd21029a29f316bf5b78d1f743983816f
angelayalafinance <96507984+angelayalafinance@users.noreply.github.com>
2022-03-21 11:18:55 -0400

    Update sentimentanalyzer.py

--- sentimentanalyzer.py
+++ sentimentanalyzer.py
@@ -1,36 +1,70 @@
-
-
-
-
-
-
-
-#   Function that parses file into words, passing the file as the argument
+from itertools import count
+from xml.etree.ElementTree import tostringlist
 from numpy import array
+from pandas import read_csv
+
+#   Import positive word dictionary
+positive = read_csv('/Users/angel/Downloads/Loughran_Mcdonald_word_list/LoughranMcDonald_Positive.csv', delimiter = ',')
+
+#   convert words to lowercase
+positive = positive.values.ravel().tolist()
+
+for i in range(len(positive)):
+    positive[i] = positive[i].lower()
 
 
-def filereader(articles):
-    with open(articles) as file:
-    # reading each line    
-    for line in file:
-   
-        # reading each word        
-        for word in line.split():
-   
-          #populate into an array
+
+# Populate a list of file paths
+path = ('/Users/angel/Downloads/amgen1.rtf')
+
+
+
+
+
+#   Function that parses word file into a words
+def filereader(path):
+    array = []
+    article = open(path, 'r')
+    for words in article.read().split():
+        array.append(words)
+    return array
+    
 
 #   Function that counts number of positive words, passing word file array
 def scorecount(arr):
-    for i in array:
-        # compare word with words
-            # increment count
-
-        # return integer
-
+    score = 0
+    for i in arr:
+        score += positive.count(i)
+    return score
 
 
 
+#   Create a company class object
+class Stock:
+  def __init__(self, name, ticker, score, performance, path):
+    #
+    #
+    #
+    #
+
+#   Store class objects in a list
+
+
+#   Iteratively update stock score
+arr = []
+for company in path:
+    arr = filereader(path)
+    sentimentscore = scorecount(arr)
+
+    
+#   Calculate stock performance    
+    
+    
+    
+    
+#   Import old stock portfolio
 
 
 
-#   Choose the top 5 stocks
+
+#   Compare performance
